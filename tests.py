a3af501fa90f9a3938b4b132bdfebbd8e913498e
Brandon DeRosier <x@bdero.me>
2015-11-14 17:21:19 -0500

    Add tests for 3Sum

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from problems import two_sum
+from problems import two_sum, three_sum
 
 
 class GenericSolutionTest(unittest.TestCase):
@@ -15,7 +15,7 @@ class GenericSolutionTest(unittest.TestCase):
 class TwoSum(GenericSolutionTest):
     module = two_sum
 
-    def test_something(self):
+    def test_two_sum(self):
         self.assertTupleEqual(
             self.solution.twoSum([2, 7, 11, 15], 9),
             (1, 2)
@@ -23,6 +23,19 @@ class TwoSum(GenericSolutionTest):
         self.assertTupleEqual(
             self.solution.twoSum([3, 2, 4], 6),
             (2, 3)
+        )
+
+
+class ThreeSum(GenericSolutionTest):
+    module = three_sum
+
+    def test_empty(self):
+        self.assertListEqual(self.solution.threeSum(None), [])
+
+    def test_simple(self):
+        self.assertListEqual(
+            self.solution.threeSum([-1, 0, 1, 2, -1, -4]),
+            [(-1, -1, 2), (-1, 0, 1)]
         )
 
 
